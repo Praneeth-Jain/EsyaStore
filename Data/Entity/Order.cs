2e460e7bff7f3b4a32d2233124a1182bb80799e8
Praneeth Jain <praneethjain15@gmail.com>
2024-11-05 16:10:21 +0530

    Initial Project Skeleton Commit

---
+++ Data/Entity/Order.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EsyaStore.Data.Entity
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string OrderNo { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        public DateTime OrderDate { get; set; }= DateTime.Now;
+
+        public int OrderStatus { get; set; } = 1;
+
+    }
+}
